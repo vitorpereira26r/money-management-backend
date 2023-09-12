f623c35036853b8ed1b79002cc80e3fd42785ec7
Vitor Pereira <vitorpereiraresende26@gmail.com>
2023-09-12 15:19:04 -0300

    repository created

---
+++ src/test/java/com/vitorpereira/money_management/MoneyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorpereira.money_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

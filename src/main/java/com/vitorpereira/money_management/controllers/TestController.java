c8825c3e122a467794127134649a8351e0da3ef8
Vitor Pereira <vitorpereiraresende26@gmail.com>
2023-09-12 16:01:07 -0300

    project set up, API test created

---
+++ src/main/java/com/vitorpereira/money_management/controllers/TestController.java
@@ -0,0 +1,18 @@
+package com.vitorpereira.money_management.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/test")
+@CrossOrigin("*")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<String> test(){
+        return ResponseEntity.ok("Ok");
+    }
+}

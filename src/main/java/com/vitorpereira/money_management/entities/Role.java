50398d0c2aac6c143212849cd35549c78c46a1bd
Vitor Pereira <vitorpereiraresende26@gmail.com>
2023-09-13 14:15:57 -0300

    user and role entity created

---
+++ src/main/java/com/vitorpereira/money_management/entities/Role.java
@@ -0,0 +1,44 @@
+package com.vitorpereira.money_management.entities;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private Integer id;
+
+    private String authority;
+
+    public Role(){}
+
+    public Role(String authority) {
+        this.authority = authority;
+    }
+
+    public Role(Integer id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+}

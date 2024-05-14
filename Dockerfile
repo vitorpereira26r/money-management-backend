e5bc05aa8637eff2870af77f5442a8cc6082a383
Vitor Pereira Resende <vitorpereiraresende26@gmail.com>
2024-05-14 11:41:23 -0300

    DockerFile updated

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM maven:3.8.3-openjdk-17 AS build
 COPY . .
 RUN mvn clean package -DskipTests
 
-FROM openjdk:17.0.1-jdk-slim
+FROM openjdk:17-jdk-alpine
 COPY --from=build /target/money_management-0.0.1-SNAPSHOT.jar app.jar
 EXPOSE 8080
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/vitorpereira/money_management/controllers/AuthenticationController.java
+++ src/main/java/com/vitorpereira/money_management/controllers/AuthenticationController.java
@@ -17,6 +17,11 @@ public class AuthenticationController {
     @Autowired
     private AuthenticationService service;
 
+    @GetMapping("/test/{name}")
+    public ResponseEntity<String> test(@PathVariable String name){
+        return ResponseEntity.ok("Hello " + name);
+    }
+
     @PostMapping(value = "/register")
     public ResponseEntity<UserApplication> register(@RequestBody RegisterUserDto registerDto){
         UserApplication user = service.registerUser(registerDto);

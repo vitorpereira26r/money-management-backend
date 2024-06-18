3ab00804ad757a8f1eb8b7dc33955c9f51e4ed92
Vitor Pereira Resende <vitorpereiraresende26@gmail.com>
2024-06-18 14:36:21 -0300

    ready to deploy into my server in the digital ocean

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,14 @@
-
-FROM maven:3.8.3-openjdk-17 AS build
+# create the .jar file
+FROM maven:3.8.4-openjdk-17 AS build
+WORKDIR /app
+COPY pom.xml .
+RUN mvn dependency:go-offline
 COPY . .
 RUN mvn clean package -DskipTests
 
+# run the application
 FROM openjdk:17-jdk-alpine
-COPY --from=build /target/money_management-0.0.1-SNAPSHOT.jar app.jar
+WORKDIR /app
+COPY --from=build /app/target/money_management-0.0.1-SNAPSHOT.jar /app/app.jar
 EXPOSE 8080
-ENTRYPOINT ["java","-jar","/app.jar"]
+ENTRYPOINT ["java","-jar","/app/app.jar"]
--- src/main/java/com/vitorpereira/money_management/configuration/CorsConfig.java
+++ src/main/java/com/vitorpereira/money_management/configuration/CorsConfig.java
@@ -21,8 +21,8 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5173");
-        config.addAllowedOrigin("https://noteworthyfinance.com");
+        //config.addAllowedOrigin("http://localhost:5173");
+        //config.addAllowedOrigin("https://noteworthyfinance.com");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
